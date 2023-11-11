5d98cb82acabcad619d5725191b14c0341c6335e
Aarón Espasandín <aaespas@gmail.com>
2023-11-11 13:12:06 +0100

    Solving BST closest value exercise

--- BST/001.Closest-Value/sol.py
+++ BST/001.Closest-Value/sol.py
@@ -1,10 +1,3 @@
-# BST Closest Value
-
-![BST Closest Value](https://github.com/aaronespasa/competitive-programming/blob/main/BST/001.Closest-Value/001.png)
-
-Solution:
-
-```py
 def findClosestValueInBst(tree, target):
 	return findClosestValueInBstHelper(tree, target, tree.value)
 		
@@ -30,10 +23,3 @@ class BST:
         self.value = value
         self.left = None
         self.right = None
-```
-
-The `findClosestValueInBst` function has a time complexity of <> and a space complexity of <>.
-
-1. **Time Complexity**: <>
-
-2. **Space Complexity**: <>
